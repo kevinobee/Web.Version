8be51e4a4a3c1c4388e60f750709d3995132f56a
kevinobee <kevin.obee@gmail.com>
2012-06-09 15:51:49 +0100

    Test with ASP.NET v3.5

--- src/test/integration/ES.Web.Version.Net35.Website/Properties/AssemblyInfo.cs
+++ src/test/integration/ES.Web.Version.Net35.Website/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ES.Web.Version.Net2.Website")]
+[assembly: AssemblyTitle("ES.Web.Version.Net35.Website")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ES.Web.Version.Net2.Website")]
+[assembly: AssemblyProduct("ES.Web.Version.Net35.Website")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("50565c34-6a24-4a13-867c-906d952fe6ac")]
+[assembly: Guid("8c8fc832-db7e-4b3f-8a7e-18e5337c734f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.5.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
